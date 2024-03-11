681a9ae985bc12d8a9bdf2e97d500cb77dac4407
Thales Rassi <thalesrassi.dev@gmail.com>
2024-03-11 15:57:43 -0300

    First class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,14 @@
 package application;
 
+import boardGame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-`		System.out.println("Hello world");
+		
+		Position pos = new Position(3,6);
+		System.out.println(pos);
+		
 	}
 
 }
